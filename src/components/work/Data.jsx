71f7cef5b7a7a37c9443003b634da938986c33a4
Mustafa Göleç <mustafagolec36@gmail.com>
2024-05-04 18:18:11 +0300

    update-1

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -8,38 +8,46 @@ export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: 'Web Design',
-        category: 'web',
+        title: 'To Do App',
+        category: 'application',
+        link: 'https://github.com/mustafagolec/React-To-Do-App',
     },
     {
         id: 2,
         image: Work2,
-        title: 'App movil',
+        title: 'Golec Aricilik',
         category: 'web',
+        link: 'https://github.com/mustafagolec/React-To-Do-App',
     },
     {
         id: 3,
         image: Work3,
         title: 'Brand design',
         category: 'game',
+        link: 'https://github.com/mustafagolec/React-To-Do-App',
     },
     {
         id: 4,
         image: Work4,
         title: 'App movil',
         category: 'game',
+        link: 'https://github.com/mustafagolec/React-To-Do-App',
     },
     {
         id: 5,
         image: Work5,
         title: 'Web Design',
         category: 'game',
+        link: 'https://github.com/mustafagolec/React-To-Do-App',
     },
 ];
 
 export const projectsNav = [
     {
         name: "all",
+    },
+    {
+        name: "application",
     },
     {
         name: "web",
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -6,7 +6,7 @@ const Work = () => {
   return (
     <section className="work section" id="portfolio">
         <h2 className="section__title">Portfolio</h2>
-        <span className="section__subtitle">My best Front-End projects</span>
+        <span className="section__subtitle">My best projects</span>
 
         <Works />
     </section>
